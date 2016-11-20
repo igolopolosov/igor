68bef76fe87be0638811a5df74a927f6a8c41c06
Igor <igolopolosov@gmail.com>
2016-11-20 18:52:35 +0300

    fix[routes] use hashHistory
    Because we host it on gihub.pages

--- scripts/components/AppContainer.js
+++ scripts/components/AppContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, IndexRoute, Route, browserHistory } from 'react-router';
+import { Router, IndexRoute, Route, hashHistory } from 'react-router';
 
 import { App } from './App';
 import { Home } from './Home';
@@ -9,7 +9,7 @@ import { NoMatch } from './NoMatch';
 export class AppContainer extends React.Component {
   render() {
     return (
-      <Router history={browserHistory}>
+      <Router history={hashHistory}>
         <Route path="/" component={App}>
           <IndexRoute component={Home}/>
           <Route path="/random-book-line" component={RandomBookLine}/>
