88a91648890766aa0d63c8af32e80868bd16ef3b
Igor <igolopolosov@gmail.com>
2017-03-17 15:57:01 +0300

    update[styles] refactoring + add styles for tablets

--- scripts/components/Home.js
+++ scripts/components/Home.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export const Home = () => (
 	<div>
-				<h1>
-					{'Hello, world!'}
-				</h1>
-				<p>
-					{"My name is Igor and it's my laboratory."}
-				</p>
-			</div>
+		<h1>
+			{'Hello, world!'}
+		</h1>
+		<p>
+			{"My name is Igor and it's my laboratory."}
+		</p>
+	</div>
 )
