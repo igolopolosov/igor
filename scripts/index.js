f09c1e58059563e52746e362a76000ca64980eed
usehotkey <igolopolosov@gmail.com>
2017-10-22 20:34:24 +0300

    [feature] console greetings

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,8 @@
 import { configureApp } from './configureHotLoader'
 import './assets'
 
+console.log('=============================================================================')
+console.log('= Hello, Dear friend! Feel free to write me email to igolopolosov@gmail.com =')
+console.log('=============================================================================')
+
 configureApp()
