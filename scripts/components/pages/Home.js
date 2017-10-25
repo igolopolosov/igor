9cdc2976ba69e0e4033c89dffce42f8c352ebcc2
usehotkey <igolopolosov@gmail.com>
2017-10-25 10:20:57 +0300

    [feature] add avatar on the Home page

--- scripts/components/pages/Home.js
+++ scripts/components/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import styles from './Home.css'
 
 export const Home = () => (
 	<div>
 		<h1>Hello, World! It's my laboratory.</h1>
+		<div className={styles.avatar} />
 		<p>
 			My name is <b>Igor Golopolosov</b> and I'm Fronted Developer at the <a target='_blank' href='https://www.spotware.com/'>Spotware</a>
 		</p>
