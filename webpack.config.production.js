d41b299f4d40c05edc4e84fb9221fd83b4c41ad5
usehotkey <igolopolosov@gmail.com>
2018-08-02 22:43:07 +0100

    Arrange production build config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -20,17 +20,12 @@ config.plugins = [
     new MiniCssExtractPlugin({
         filename: "[name].css",
         chunkFilename: "[id].css"
-    }),,
+    }),
     new webpack.DefinePlugin({
         'process.env': {
             'NODE_ENV': JSON.stringify('production')
         },
         PRODUCTION: JSON.stringify(true)
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: true
-        }
     }),
     new HtmlWebpackPlugin({
         template: 'index.ejs',
