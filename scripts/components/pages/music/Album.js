c35cddbd45b5f817a870691b8f41cfae7e78d1d2
usehotkey <igolopolosov@gmail.com>
2018-08-28 22:30:35 +0100

    [music] Add variants of cover and soundcloud iframe

--- scripts/components/pages/music/Album.js
+++ scripts/components/pages/music/Album.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import styles from './album.css'
 
-const albums = [
+export const albums = [
+	{
+		band: 'сиг?',
+		album: 'обогнавшие скорость!',
+		date: '2015',
+		link: 'https://soundcloud.com/hotkeymusic/sets/sig-speed',
+		soundloud: '260868019',
+		coverUrl: 'https://i1.sndcdn.com/artworks-000186691930-8flxpa-t500x500.jpg',
+		tracks: [
+			'обогнавшие скорость!'
+		]
+	},
+	{
+		band: 'Nothing Personal',
+		album: 'Nothing Wasted',
+		date: '2015',
+		link: 'https://soundcloud.com/hotkeymusic/sets/nothing-personal-nothing-wasted',
+		soundloud: '264803944',
+		coverUrl: 'https://i1.sndcdn.com/artworks-000186700926-w0ar73-t500x500.jpg',
+		tracks: [
+			'My Birthday',
+			`When I'm Falling`,
+			'Nevermind'
+		]
+	},
 	{
 		band: '2 a.m.',
 		album: 'Creator / Weekend',
 		date: '2014',
 		link: 'https://soundcloud.com/hotkeymusic/sets/2-a-m-creator-weekend',
+		soundloud: '264798475',
 		coverUrl: 'https://i1.sndcdn.com/artworks-000186694391-co76nh-t500x500.jpg',
 		tracks: [
 			'Creator',
@@ -18,6 +43,7 @@ const albums = [
 		album: 'Verve',
 		date: '2014',
 		link: 'https://soundcloud.com/hotkeymusic/sets/2-a-m-verve',
+		soundloud: '264798404',
 		coverUrl: 'https://i1.sndcdn.com/artworks-000186694163-01cv6e-t500x500.jpg',
 		tracks: [
 			'Verve'
@@ -25,30 +51,72 @@ const albums = [
 	}
 ]
 
-export const Album = ({
-	band,
-	album,
-	date,
-	link,
-	coverUrl,
-	rating,
-	tracks
-}) => (
-	<div
-		className={styles.album}
-		key={album}
-	>
-		<a
-			className={styles.info}
-			target="_blank"
-			href={link}
-		>
-			<h4>{band}</h4>
-			<h5>{album}</h5>
-			<h6>{date}</h6>
-		</a>
-		<img src={coverUrl} className={styles.coverUrl} />
-	</div>
-)
+const iframeSettings = {
+	width: "100%",
+	height: "300",
+	scrolling: "no",
+	frameBorder: "no",
+	allow: "autoplay"
+}
 
-// {albums.map(album => <Album {...album} />)}
+function getSouncloudIframeSrc(id) {
+	const sc0 = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/"
+	const sc1 = "&color=%23ff5500&auto_play=false&hide_related=false"
+	const sc2 = "&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+
+	return `${sc0}${id}${sc1}${sc2}`
+}
+
+export class Album extends React.Component {
+
+	constructor() {
+		super()
+		this.state = {
+			isPlayerDisplayed: false
+		}
+	}
+
+	onHover(nextState) {
+		this.setState({ isPlayerDisplayed: nextState })
+	}
+
+	render() {
+		const {
+			band,
+			album,
+			date,
+			link,
+			soundloud,
+			coverUrl,
+			rating,
+			tracks
+		} = this.props
+		const { isPlayerDisplayed } = this.state
+
+		return (
+			<div
+				className={styles.album}
+				onMouseEnter={() => this.onHover(true)}
+				onMouseLeave={() => this.onHover(false)}
+			>
+				<img src={coverUrl} className={styles.coverUrl} />
+				<a
+					className={styles.info}
+					style={{ display: isPlayerDisplayed && 'none' }}
+					target="_blank"
+					href={link}
+				>
+					<h4>{band}</h4>
+					<h5>{album}</h5>
+					<h6>{date}</h6>
+				</a>
+				<iframe
+					{...iframeSettings}
+					className={styles.iframe}
+					style={{ display: !isPlayerDisplayed && 'none' }}
+					src={getSouncloudIframeSrc(soundloud)}
+				/>
+			</div>
+		)
+	}
+}
--- scripts/components/pages/music/Music.js
+++ scripts/components/pages/music/Music.js
@@ -1,25 +1,9 @@
 import React from 'react'
+import {Album, albums} from './Album.js'
 import styles from './music.css'
-
-// {albums.map(album => <Album {...album} />)}
-
-const iframeSettings = {
-	width: "100%",
-	height: "300",
-	scrolling: "no",
-	frameBorder: "no",
-	allow: "autoplay"
-}
-
-const scLink = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/264798475"
-const sc1 = "&color=%23ff5500&auto_play=false&hide_related=false"
-const sc2 = "&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
 
 export const Music = () => (
 	<div className={styles.main}>
-		<iframe
-			{...iframeSettings}
-			src={`${scLink}${sc1}${sc2}`}
-		></iframe>
+		{albums.map(album => <Album {...album} key={album.album} />)}
 	</div>
 )
