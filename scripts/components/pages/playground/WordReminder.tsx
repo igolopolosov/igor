741c4e67bfb1007d060e46a9fe9f6b862fa28e6f
usehotkey <igolopolosov@gmail.com>
2019-02-03 21:55:45 +0000

    [word reminder] Implement simple word edtior

--- scripts/components/pages/playground/WordReminder.tsx
+++ scripts/components/pages/playground/WordReminder.tsx
@@ -1,11 +1,80 @@
 import * as React from 'react';
+import { Button } from '../../blocks/Button';
+const styles = require('./WordReminder.css');
 
-export class WordReminder extends React.PureComponent {
+interface WordReminderState {
+    newWord: string,
+    newSentence: string,
+    store: {
+        word: string,
+        sentence: string
+    }[],
+}
+
+export class WordReminder extends React.PureComponent<{}, WordReminderState> {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            newWord: '',
+            newSentence: '',
+            store: []
+        }
+    }
+
+    enterWord = (event) => {
+        event.persist()
+        
+        this.setState(() => ({
+            newWord: event.target.value
+        }));
+    }
+
+    enterSentence = (event) => {
+        event.persist()
+        
+        this.setState(() => ({
+            newSentence: event.target.value
+        }));
+    }
+
+    addWord = () => {
+        this.setState((state) => ({
+            store: [
+                ...state.store,
+                { 
+                    sentence: state.newSentence,
+                    word: state.newWord
+                }
+            ]
+        }));
+    }
 
     render() {
+        const { store } = this.state;
+
         return (
             <div>
-                <h1>Hi</h1>
+                <h3>Add words to reminder</h3>
+                
+                <p>
+                    <input placeholder='Type word' onBlur={this.enterWord}/>
+                    <input placeholder='Type sentence' onBlur={this.enterSentence}/>
+                </p>
+
+                <p>
+                    <Button tertiary onClick={this.addWord}>
+                        Add
+                    </Button>
+                </p>
+
+                <p className={styles.reminderList}>
+                    {store.map(item => {
+                        const html = item.sentence.replace(new RegExp(item.word, "g"), `<b>${item.word}</b>`);
+                        return <div key={Math.random()} dangerouslySetInnerHTML={{ __html: html }} />
+                    })}
+                </p>
             </div>
         );
     }
