0a73864680822b9a33f41bcbc16f864ce7447f95
Igor Golopolosov <igolopolosov@gmail.com>
2019-04-27 23:49:44 +0100

    [fix] Migrate articles from Yandex to Github
    In order to prevent access difficulties the articles was moved to Github

--- scripts/components/pages/blog/loadBlog.ts
+++ scripts/components/pages/blog/loadBlog.ts
@@ -1,25 +1,9 @@
-import { BlogPost, setBlogPosts } from '../../../reducers/blog';
-import { ActionWithPayload } from '../../../helpers/redux';
-
-const YANDEX_API_LINK = 'https://cloud-api.yandex.net:443/v1/disk/public/resources/download';
-const PUBLIC_KEY = 'https%3A%2F%2Fyadi.sk%2Fd%2FS16MSRKVz4uRV';
-const BLOG_PATH = '/blog.json';
-const BLOG_URL = `${YANDEX_API_LINK}?public_key=${PUBLIC_KEY}&path=${BLOG_PATH}`;
-
-const makeJSON = r => r.json();
+import { setBlogPosts } from '../../../reducers/blog';
+import { data } from './articles';
 
 /**
- * Load posts from Yandex blog
+ * Load posts from local file
  */
-export function loadBlog(): Promise<ActionWithPayload<BlogPost[]>> {
-    return fetch(BLOG_URL, {method: 'GET'})
-        .then(makeJSON)
-        .then(r => {
-            if (r.error) {
-                throw r;
-            }
-            return fetch(r.href, {method: 'GET'});
-        })
-        .then(makeJSON)
-        .then(setBlogPosts);
+export function loadBlog() {
+    return setBlogPosts(data);
 }
