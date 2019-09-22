4c9ace95e5baf2036f96b467627b3d90d5c6a7ee
Igor Golopolosov <igolopolosov@gmail.com>
2019-09-22 14:43:23 +0100

    [ci] add github username and email

--- release.sh
+++ release.sh
@@ -2,6 +2,9 @@ echo "⭐️ Clone igolopolosov.github.io"
 cd ../
 git clone https://github.com/igolopolosov/igolopolosov.github.io
 
+git config --global user.email "igolopolosov@gmail.com"
+git config --global user.name "Igor Golopolosov"
+
 echo "⭐️ Clean old files"
 cd ./igolopolosov.github.io
 rm bundle*
