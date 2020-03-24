cd4f5d2e0207de4e85392ccdc85c381ceee8cbb6
Igor Golopolosov <igolopolosov@gmail.com>
2020-03-24 22:13:11 +0000

    :fire: Remove language stickers

--- scripts/components/pages/home/Home.js
+++ scripts/components/pages/home/Home.js
@@ -1,13 +1,11 @@
 import React from "react";
 import styles from "./Home.less";
 
-import { Stickers } from "./Stickers";
 import { CTM } from "./CTM";
 
 export const Home = () => (
   <>
     <h2>Hello, World! It&apos;s my laboratory.</h2>
-    <Stickers />
     <div className={styles.avatar} />
     <div className={styles.intro}>
       <b>Igor Golopolosov.</b> Senior Software Enginner at <CTM />
